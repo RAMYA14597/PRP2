e0fc29bd77bef3b24a516241a2f6aecec8120166
RAMYA14597 <ramyaerusan@gmail.com>
2019-10-01 00:58:42 -0700

    Update reverse_word.java

--- reverse_word.java
+++ reverse_word.java
@@ -8,11 +8,17 @@ Scanner sc=new Scanner(System.in);
 String S=sc.nextLine();
 int i;
 String out="";
+if(S <=10000000)
+{
 String S1[]=S.split(" ");
 for(i=S1.length-1;i>=0;i--)
 {
 out=out+S1[i]+" ";
 }
 System.out.print(out);
+}
+  else
+  {
+    System.out.println("limit exceed");
 }
 }
