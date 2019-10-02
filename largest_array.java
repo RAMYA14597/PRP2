8f55a200f6a24da4206ed1d403d6415640862591
RAMYA14597 <ramyaerusan@gmail.com>
2019-10-02 01:33:56 -0700

    Update largest_array.java

--- largest_array.java
+++ largest_array.java
@@ -8,12 +8,23 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 int count=sc.nextInt();
 int list[]=new int[count];
+int tmp[]=new int[count];
 int i;
+if(count>=1 && count<=10)
+{
 for(i=0;i<count;i++)
 {
 list[i]=sc.nextInt();
 }
-Arrays.sort(list);
-System.out.println(list[count-1]);
+}
+for(i=0;i<count;i++)
+{
+if(list[i]>=0 && list[i]<=100)
+{
+tmp[i]=list[i];
+}
+}
+Arrays.sort(tmp);
+System.out.println(tmp[count-1]);
 }
 }
