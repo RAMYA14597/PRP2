d3e84dbc241246e38c8639a7dcfc6abac06e0676
RAMYA14597 <ramyaerusan@gmail.com>
2019-10-02 01:12:49 -0700

    Update odd_print.java

--- odd_print.java
+++ odd_print.java
@@ -6,19 +6,20 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 String N=sc.next();
-int i;
+int i,ch;
 String res="";
 if(N.length()<=100000)
 {
 for(i=0;i<N.length();i++)
 {
-if(i%2!=0)
+ch=Character.getNumericValue(N.charAt(i));
+if(ch%2!=0)
 {
-res=res+N.charAt(i)+" ";
+res=res+ch+" ";
 }
 }
 }
-if(N.length()==1)
+if(res.length()==0)
 {
 System.out.println("-1");
 }
